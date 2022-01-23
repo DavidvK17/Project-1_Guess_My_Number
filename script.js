50b4f5be0d3b7d6d5b8d1fd443dc5034b08a1c2f
DavidvK17 <david.sing7@gmail.com>
2022-01-23 21:54:59 +0100

    update score and css animation on win

--- script.js
+++ script.js
@@ -4,7 +4,11 @@
 const randomNum = Math.trunc(Math.random() * 20 + 1);
 console.log(randomNum);
 // Hidden Number
-const hiddenNumber = document.getElementById('random--number');
+const hiddenNumber = document.getElementsByClassName('random--number');
+
+// storing score value
+let score = 20;
+const displayScore = document.getElementById('score');
 
 // get user input
 const userGuess = document.getElementById('user--guess');
@@ -20,14 +24,24 @@ checkUserGuess.addEventListener('click', function(e) {
   userGuess.value = '';
 
   //check if number is between 0 and 20 and === generated number
+  //   countdown score
   if (guess > 0 && guess < 20) {
     if (guess === randomNum) {
       text.textContent = '🏆 Correct Number!';
-      hiddenNumber.textContent = randomNum;
+      hiddenNumber[0].textContent = `${randomNum}`;
+      hiddenNumber[0].classList.add('is--correct');
+      document.getElementsByTagName('h1')[0].textContent = '🏆 You Win! :)';
     } else if (guess > randomNum) {
       text.textContent = 'Too High!';
+      calcDispScore();
     } else if (guess < randomNum) {
       text.textContent = 'Too Low!';
+      calcDispScore();
     }
+  }
+
+  function calcDispScore() {
+    score--;
+    displayScore.textContent = `${score}`;
   }
 });
