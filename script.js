cecaf622e1a89c931a3e983ec7436f3d0e187864
DavidvK17 <david.sing7@gmail.com>
2022-01-23 19:05:54 +0100

    Implementing HTML Form and getting user input

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// generate random num between 0 and 20 on documentload
+const randomNum = Math.trunc(Math.random() * 20 + 1);
+console.log(randomNum);
+
+// get user input
+const userGuess = document.getElementById('user--guess');
+const checkUserGuess = document.getElementById('check--user--guess');
+
+checkUserGuess.addEventListener('click', function(e) {
+  e.preventDefault();
+  //   comvert user input to Number
+  Number(userGuess.value);
+  userGuess.blur();
+  //   clear input field
+  userGuess.value = '';
+});
